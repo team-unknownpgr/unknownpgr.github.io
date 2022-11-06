d6cbe1ba3e086e080045cdf9f7b067e2430af14d
Joonho Gwon <unknownpgr@gmail.com>
2022-11-06 03:48:21 +0000

    Implement post list

---
+++ frontend-next/components/postList/postList.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import styles from "./postList.module.scss";
+
+interface IMetadata {
+  [key: string]: {
+    title: string;
+    category: string;
+    name: string;
+    date: string;
+  };
+}
+
+function formatDate(d: Date, displayTime = false) {
+  let ret =
+    ("0" + d.getUTCDate()).slice(-2) +
+    "-" +
+    ("0" + (d.getUTCMonth() + 1)).slice(-2) +
+    "-" +
+    d.getFullYear();
+  if (displayTime) {
+    ret +=
+      " " +
+      ("0" + d.getUTCHours()).slice(-2) +
+      ":" +
+      ("0" + d.getUTCMinutes()).slice(-2);
+  }
+  return ret;
+}
+
+export default function PostList({ metadata }: { metadata: IMetadata }) {
+  return (
+    <div className={styles.list}>
+      {Object.values(metadata).map(({ name, title, date, category }) => (
+        <Link key={name} href={`/posts/${name}`}>
+          <div className={styles.post}>
+            <div>
+              <strong>{title}</strong>
+            </div>
+            <div className={styles.metadata}>
+              {formatDate(new Date(date))} / {category}
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
--- frontend-next/pages/index.tsx
+++ frontend-next/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/header/header";
+import PostList from "../components/postList/postList";
 import metadata from "../meta.json";
 
 export default function Home() {
@@ -12,21 +12,7 @@ export default function Home() {
         <link rel="icon" href="/logo.png" />
       </Head>
       <Header />
-      <hr />
-      {Object.values(metadata).map(
-        ({ name, title, date, category, thumbnail }) => (
-          <div key={name}>
-            <Image
-              src={thumbnail || "/code.png"}
-              alt={title}
-              width={320}
-              height={240}
-            ></Image>
-            {title}
-            {}
-          </div>
-        )
-      )}
+      <PostList metadata={metadata} />
     </div>
   );
 }
